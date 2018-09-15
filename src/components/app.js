afdc976873130988676cb58c4e695437167bc859
Marcos Scorzoni <mlessa@gmail.com>
2018-09-15 11:04:49 +0100

    add constructor to app class

--- src/components/app.js
+++ src/components/app.js
@@ -1,7 +1,26 @@
 import React, { Component } from 'react';
 import SearchBar from './search_bar';
+import YTSearch from 'youtube-api-search';
+import REACT_APP_GOOGLE_API_KEY from '../.././env';
+
+
+const API_KEY = REACT_APP_GOOGLE_API_KEY;
 
 export default class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = { videos: [] }; 
+
+    YTSearch({key: API_KEY, term: 'surfboars'}, (videos) => {
+      this.setState({ videos });
+      // this.setState({ videos: videos });
+    });
+
+
+  }
+
+
   render() {
     return (
       <div>
