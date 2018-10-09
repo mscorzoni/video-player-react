c807597f154f1e4e399c8e14d68711b8d3d3716e
Marcos Scorzoni <mlessa@gmail.com>
2018-10-09 15:52:14 +0100

    add home router

--- src/index.js
+++ src/index.js
@@ -2,18 +2,27 @@ import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
-import App from './components/app';
 import reducers from './reducers';
+import promise from 'redux-promise';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import PostsIndex from './components/posts_index';
+import PostsNew from './components/posts_new';
 
 
 
 
-const createStoreWithMiddleware = applyMiddleware()(createStore);
-
+const createStoreWithMiddleware = applyMiddleware(promise)(createStore);
 
 
 ReactDOM.render(
   <Provider store={createStoreWithMiddleware(reducers)}>
-    <App />
+    <BrowserRouter>
+      <div>
+        <Switch>
+          <Route path="/posts/new" component={PostsNew} />
+          <Route path="/" component={PostsIndex} />
+        </Switch>
+      </div>
+    </BrowserRouter>
   </Provider>
   , document.querySelector('.container'));
